8cedc2c92f9ca36e7e379c290fb2a4a0e5cdb2e4
Gagandeep Singh <g9singh@ucsd.edu>
2018-11-08 23:41:03 -0800

    Initial Commit

---
+++ Lab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab
+//
+//  Created by Singh, Gagandeep on 11/8/18.
+//  Copyright © 2018 Singh, Gagandeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
